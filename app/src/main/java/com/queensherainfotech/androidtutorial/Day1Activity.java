5c3c7517f78cb2cc49e98bc7540b693944dc6456
Queenshera Infotech <queensherainfotech@gmail.com>
2020-05-16 00:32:48 +0530

    Day 3 activity commit and previous minor changes

--- app/src/main/java/com/queensherainfotech/androidtutorial/Day1Activity.java
+++ app/src/main/java/com/queensherainfotech/androidtutorial/Day1Activity.java
@@ -14,7 +14,6 @@ public class Day1Activity extends AppCompatActivity {
     TextView txvDisplay;
     EditText edtInput;
     Button btnClickMe;
-    Button btnNextPage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,17 +23,11 @@ public class Day1Activity extends AppCompatActivity {
         txvDisplay = findViewById(R.id.txvDisplay);
         edtInput = findViewById(R.id.edtInput);
         btnClickMe = findViewById(R.id.btnClickMe);
-        btnNextPage = findViewById(R.id.btnNextPage);
 
         btnClickMe.setOnClickListener(view -> {
             String text = edtInput.getText().toString();
             txvDisplay.setText(text);
             Toast.makeText(this, "button single click", Toast.LENGTH_SHORT).show();
-        });
-
-        btnNextPage.setOnClickListener(v->{
-            Intent intent = new Intent(Day1Activity.this,Day2Activity.class); //first parameter current activity and second parameter new activity
-            startActivity(intent);
         });
     }
 }
